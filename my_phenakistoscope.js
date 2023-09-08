aa831dcddd4841dcf809d9a77214ee1dce7d51a3
baolong2005 <135151094+baolong2005@users.noreply.github.com>
2023-09-08 13:35:11 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -2,6 +2,7 @@ const SLICE_COUNT = 10;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
+  //pScope.output_mode(STATIC_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(true);
   pScope.set_direction(CCW);
@@ -10,40 +11,28 @@ function setup_pScope(pScope){
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, 0);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  var layer1 = new PLayer(circles);
+  layer1.mode( SWIRL(3) );
+  layer1.set_boundary( 0, 1000 );
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
 }
 
-function faces(x, y, animation, pScope){
+function circles(x, y, animation, pScope){
   
   scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
-
-}
-
-function squares(x, y, animation, pScope){
-
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
-
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  blendMode(DIFFERENCE);
+  fill(255, 230, 0);
+  ellipse(0,0,120,120);
+  fill(8, 0, 255); //blue
+  translate(-40,80);
+  ellipse(0,0,120,120);
+  translate(-40,80);
+  fill(255, 0, 0); //red
+  ellipse(0,0,120,120);
+   
 
 }
+
+
