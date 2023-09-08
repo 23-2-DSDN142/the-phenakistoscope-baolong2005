7d3e616767419546a1b9234071bc8abc7b640b1f
baolong2005 <135151094+baolong2005@users.noreply.github.com>
2023-09-08 14:16:51 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,10 +1,10 @@
 const SLICE_COUNT = 10;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_GIF(1000));
   //pScope.output_mode(STATIC_DISK);
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
